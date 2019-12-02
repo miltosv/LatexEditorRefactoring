29e0faad9233b4feb678c5c319209bf2a7e62e77
ThThoma <36672286+ThThoma@users.noreply.github.com>
2019-12-02 11:22:23 +0200

    AtbashCipher done
    atbash working correctly

--- ProjectMaterial/LatexEditorProject/src/model/encryption/AtbashCipher.java
+++ ProjectMaterial/LatexEditorProject/src/model/encryption/AtbashCipher.java
@@ -1,6 +1,13 @@
 package model.encryption;
 
+
 public class AtbashCipher implements CipherStrategy {
+	
+	
+	
+	public AtbashCipher() {
+		
+	}
 
 	@Override
 	public String encrypt(String str) {
@@ -8,42 +15,53 @@ public class AtbashCipher implements CipherStrategy {
 		char strCh;
 		String encryptedString = "";
 		
-		
 		for( int i = 0; i < str.length(); i++){
 			strCh = str.charAt(i);
 			
 			if(strCh >= 'a' && strCh <= 'z'){
-	            strCh = (char)(strCh + 'a');
-	            
-	            if(strCh > 'z'){
-	            	strCh = (char)(strCh - 'z' + 'a' - 1);
-	            }
-	            
-	            encryptedString += strCh;
-	        }
-	        else if(strCh >= 'A' && strCh <= 'Z'){
-	        	strCh = (char)(strCh + 'A');
-	            
-	            if(strCh > 'Z'){
-	            	strCh = (char)(strCh - 'Z' + 'A' - 1);
-	            }
-	            
-	            encryptedString += strCh;
-	        }
-	        else {
-	        	// char other than letter
-	        	encryptedString += strCh;
-	        }
+				
+				encryptedString += (char)(( ( 25 * (strCh-'a') + 25) % 26) + 'a');
+			}
+			else if(strCh >= 'A' && strCh <= 'Z'){
+				
+				encryptedString += (char)(( ( 25 * (strCh-'A') + 25) % 26) + 'A'); 
+			}
+			else {
+				// char other than letter
+	        	encryptedString += strCh;	
+			}	
 		}
-		
-		
 		return encryptedString;
 	}
 
 	@Override
 	public String decrypt(String str) {
-		// TODO Auto-generated method stub
-		return null;
+		char strCh;
+		String decryptedString = "";
+		
+		
+		for( int i = 0; i < str.length(); i++){
+			strCh = str.charAt(i);
+			
+			if(strCh >= 'a' && strCh <= 'z'){
+	            
+				strCh= Character.toUpperCase(strCh);
+				strCh= (char) (( 25 *(strCh + 'A' - 25) % 26) + 'A');
+				strCh = Character.toLowerCase(strCh);
+				decryptedString += strCh;
+	        }
+	        else if(strCh >= 'A' && strCh <= 'Z'){
+	        	
+	            decryptedString += (char) (( 25 *(strCh + 'A' - 25) % 26) + 'A'); 
+	        }
+	        else {
+	        	// char other than letter
+	        	decryptedString += strCh;
+	        }
+		}
+		
+		
+		return decryptedString;
 	}
 
 }
